5493615150506112a08d078255bf44c0e02e7d27
AkerKratz <akerkratz@gmail.com>
2023-04-26 12:50:51 +0300

    successfull result

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,12 @@ $('#subscription_form').on('submit', function (e) {
  var $form = $(this);
  $.ajax({
  type: 'POST',
- url: '/subscription_ajax.php',
+ url: 'subscription_ajax.php',
  data: $form.serialize()
  }).done(function () {
  $form[0].reset()
  alert('Thank you for the subscription!')
  }).fail(function () {
- alert('Something went wrong')
+ alert('Something went wron11g')
  });
 });
